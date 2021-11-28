2906fadf450c87bcd1cecc71ee29559b7b78f16f
Eric <ericpoliveira@gmail.com>
2021-11-28 17:36:51 -0300

    calculo-de-desconto-atribuicao

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal, inss, pen;
+
+            Console.WriteLine("Digite o salário do funcionário.");
+            sal = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o % de INSS.");
+            inss = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o % de pensão.");
+            pen = double.Parse(Console.ReadLine());
+
+            sal -= sal*inss/100;
+            sal -= sal*pen/100;
+
+            Console.WriteLine("Salário final: "+sal.ToString("C"));
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+        }
+    }
+}
